d1a8499a648a70ea31b1603275f5d2763e209e71
levijskal <levijskal@icloud.com>
2018-09-06 09:40:18 -0400

    after 403

--- starter_code/app.js
+++ starter_code/app.js
@@ -7,12 +7,42 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('views', __dirname + '/views');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
+app.get('/beers', (req, res, next) => {
+  
+  punkAPI.getBeers()
+    .then(beers => {
 
-
-app.listen(3000);
+      
+      console.log(beers[0].food_pairing)
+      res.render('beers', {beers: beers });
+      
+    })
+    .catch(error => {
+    })
+  });
+  
+  app.get('/random-beers', (req, res, next) => {
+    res.render('randombeer');
+  });
+  
+  // console.log(error)
+  
+  app.listen(3000);
+  // res.render('beers');
+      // let beerArr = beers.map(beer => {
+      //   return {
+      //       title: beer.name,
+      //       tagline: beer.tagline,
+      //       url: beer.image_url,
+      //       description: beer.description
+      //   }
+      // });
